d5ec5e189950cad8746676a4d542803124fe4159
karen <arq.karengallegos89@gmail.com>
2021-08-02 15:22:59 +0200

    Trash can added

--- src/components/Bank.js
+++ src/components/Bank.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Card from './Card';
+import ColorNumber from './ColorNumber';
 
 export default function Bank() {
   const [item, setItem] = useState({ title: '', amount: null });
@@ -21,10 +22,7 @@ export default function Bank() {
   };
 
   const handleWithDrawClick = () => {
-    const negativeAmount = item.amount * -1;
-    const name = item.title;
-
-    setList((state) => [...state, { title: name, amount: negativeAmount }]);
+    setList((state) => [...state, { title: item.title, amount: item.amount * -1 }]);
   };
 
   //this is "derived state, or "computer" state
@@ -36,6 +34,10 @@ export default function Bank() {
   };
   const balance = () => {
     return income() + outcome();
+  };
+
+  const deleteItem = (itemToBeDeleted) => {
+    setList((element) => element.filter((item) => item !== itemToBeDeleted));
   };
 
   return (
@@ -119,7 +121,19 @@ export default function Bank() {
           {list.map((item, i) => (
             <tr key={i}>
               <td class='text-center'>{item.title}</td>
-              <td class='text-center'> {item.amount}</td>
+              <td class='text-center'>
+                <ColorNumber number={item.amount} />
+              </td>
+              <button
+                class='inline-flex items-center justify-center mb-1 mt-1 w-6 h-6 mr-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800'
+                onClick={() => deleteItem(item)}>
+                <svg class='w-4 h-4 fill-current' viewBox='0 0 20 20'>
+                  <path
+                    d='M17.114,3.923h-4.589V2.427c0-0.252-0.207-0.459-0.46-0.459H7.935c-0.252,0-0.459,0.207-0.459,0.459v1.496h-4.59c-0.252,0-0.459,0.205-0.459,0.459c0,0.252,0.207,0.459,0.459,0.459h1.51v12.732c0,0.252,0.207,0.459,0.459,0.459h10.29c0.254,0,0.459-0.207,0.459-0.459V4.841h1.511c0.252,0,0.459-0.207,0.459-0.459C17.573,4.127,17.366,3.923,17.114,3.923M8.394,2.886h3.214v0.918H8.394V2.886z M14.686,17.114H5.314V4.841h9.372V17.114z M12.525,7.306v7.344c0,0.252-0.207,0.459-0.46,0.459s-0.458-0.207-0.458-0.459V7.306c0-0.254,0.205-0.459,0.458-0.459S12.525,7.051,12.525,7.306M8.394,7.306v7.344c0,0.252-0.207,0.459-0.459,0.459s-0.459-0.207-0.459-0.459V7.306c0-0.254,0.207-0.459,0.459-0.459S8.394,7.051,8.394,7.306'
+                    clip-rule='evenodd'
+                    fill-rule='evenodd'></path>
+                </svg>
+              </button>
             </tr>
           ))}
         </tbody>
